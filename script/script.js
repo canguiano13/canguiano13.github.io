e276b931ada9ce1c6d5787684bdfe4ca014ca36c
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-06-29 20:14:31 -0700

    configure resizing main content with desktop nav

--- script/script.js
+++ script/script.js
@@ -1,6 +1,20 @@
 function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+    const nav = document.querySelector(".dropdown-nav");
+    if (nav.style.height != "auto"){
+        nav.style.height = "auto";
+    }else{
+        nav.style.height = "12vh";
+    }
+    const menu = document.querySelector(".dropdown-menu-links");
+    const icon = document.querySelector(".dropdown-icon");
+    
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+//document.addEventListener("DOMContentLoaded", toggleMenu());
