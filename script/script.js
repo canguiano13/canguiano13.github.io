1af695f9e13667658b62c02ae83259826c900fc3
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-07-11 20:21:23 -0700

    add collapse/expand ability to main sections

--- script/script.js
+++ script/script.js
@@ -1,20 +1,51 @@
+
+//collapse/expand section
+function toggleCollapsible(clickedButton, sectionId){
+    //update text/title of button
+    if(clickedButton.textContent === "\u2014"){
+        clickedButton.textContent="+";
+        clickedButton.title = "Expand section";
+    } else{
+        clickedButton.textContent="\u2014";
+        clickedButton.title = "Collapse section";
+    }
+
+    //get all children of desired section
+    elementsToToggle = document.getElementById(sectionId).querySelectorAll("h4, p, ol");
+    console.log(elementsToToggle);
+    //hide/show children
+    for (let i=0; i<elementsToToggle.length; i++){
+        sectionToClose = elementsToToggle[i];
+        sectionToClose.style.display = (sectionToClose.style.display === "none") ? "block":"none";
+    }
+}
+
+
+//TODO fix implementation
 function toggleMenu(){
     const nav = document.querySelector(".dropdown-nav");
-    if (nav.style.height != "auto"){
-        nav.style.height = "auto";
+    //change the height of the nav bar so that the links fit
+    if (nav.style.height != "24vh"){
+        nav.style.height = "24vh";
+        nav.style.width = "100%";
     }else{
         nav.style.height = "12vh";
+    }
+    
+    if (nav.style.display == "none"){
+        nav.style.display == "block";
+    }else{
+        nav.style.display == "none";
     }
     const menu = document.querySelector(".dropdown-menu-links");
     const icon = document.querySelector(".dropdown-icon");
     
-    if(menu.style.display === "block"){
+    if(menu.style.display === "inline-block"){
         menu.style.display = "none";
+        menu.style.width = "100%";
     }else{
         menu.style.display = "block";
     }
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+
 }
 
-//document.addEventListener("DOMContentLoaded", toggleMenu());
